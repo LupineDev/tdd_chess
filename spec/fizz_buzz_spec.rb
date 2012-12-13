9f4aa1e887163f22da7be6438956fa795a3d92a7
LupineDev <lupinedev@gmail.com>
2012-12-13 05:42:03 -1000

    Test to check mod 5

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -5,5 +5,9 @@ describe FizzBuzz do
     it "should return 'fizz' when passed a mulitple of 3" do
       FizzBuzz.check(6).should eq("fizz")
     end
+
+    it "should return 'buzz' when passed a mulitple of 5" do
+      FizzBuzz.check(10).should eq("buzz")
+    end
   end
 end

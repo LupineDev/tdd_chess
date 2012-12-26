91ed06ad45b6372bead924558c60a134338cbb87
LupineDev <lupinedev@gmail.com>
2012-12-26 11:34:13 -1000

    Mele Kalikimaka :)

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe FizzBuzz do
+  def test_range
+    [*0..100]
+  end
   describe ".check(num)" do
     it "should return 'fizz' when passed a mulitple of 3" do
-      FizzBuzz.check(6).should eq("fizz")
+      test_range.each do |i|
+        FizzBuzz.check(i).should eq("fizz") if i % 3 == 0
+      end
     end
 
     it "should return 'buzz' when passed a mulitple of 5" do
-      FizzBuzz.check(10).should eq("buzz")
+      test_range.each do |i|
+        FizzBuzz.check(i).should eq("buzz") if i % 5 == 0
+      end
     end
   end
 end

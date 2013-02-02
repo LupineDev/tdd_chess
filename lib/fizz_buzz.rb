480a01bd678d8f6316ffee6ccbfbfe4cde281ccd
Nick Niemeir <nick.niemeir@gmail.com>
2013-02-01 23:10:30 -0700

    Fizzbuzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,9 @@
 class FizzBuzz
 
   def self.check(num)
-    if num % 5 == 0
+    if num % 5 == 0 and num % 3 == 0
+      'fizzbuzz'
+    elsif num % 5 == 0
       'buzz'
     elsif num % 3 == 0
       'fizz'
